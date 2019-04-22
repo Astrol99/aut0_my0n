21263eb792a32bafc616dc1cc2f3084b3684caa6
Dav2006 <dngames2006@gmail.com>
2019-04-21 20:49:37 -0500

    Made verbose an option

--- aut0_myon.py
+++ aut0_myon.py
@@ -31,7 +31,7 @@ print("""
 \__,_/\__,_/\__/\____/____/_/ /_/ /_/\__, /\____/_/ /_/ 
                     /_____/         /____/          
                     
-                | Version: 1.0.0 |       
+                | Version: 1.0.1 |       
                  | By: Astrol99 | 
 
 TIP: Press Ctrl or Control + C to exit program!\n
@@ -39,6 +39,7 @@ TIP: Press Ctrl or Control + C to exit program!\n
 
 tabs = int(input("[i] Amount of tabs open: ")) + 1
 timeWait = int(input("[i] Amount of seconds per wait: "))
+verbose = str(input("[i] Turn on verbose?(Sends info about when clicked and etc.)[Y or N]: ")).upper()
 
 print("\n[!] Starting in 5 seconds...")
 
@@ -51,16 +52,20 @@ try:
         for i in range(tabs):
             if i == 0:
                 continue # Used to pass loop 0
-            print(f"\n[*] On tab: {i}...")
+            if verbose == "Y":
+                print(f"\n[*] On tab: {i}...")
             time.sleep(0.5)
             if os.name == "nt":
                 pyautogui.hotkey("ctrl", str(i))
             else:
                 pyautogui.hotkey("command" , str(i))
-            print(f"[+] Switched tabs to tab: {i}")
+            if verbose == "Y":
+                print(f"[+] Switched tabs to tab: {i}")
             pyautogui.press("right")
-            print("[+] Pressed right -> Turned page")
-        print(f"[*] On cooldown for {timeWait} secs...")
+            if verbose == "Y":
+                print("[+] Pressed right -> Turned page")
+        if verbose == "Y":
+            print(f"[*] On cooldown for {timeWait} secs...")
         time.sleep(timeWait)
 except KeyboardInterrupt:
     print("[!] Stopped! -> exiting...")
