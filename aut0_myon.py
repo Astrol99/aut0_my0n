a95011e79a9f6b154295825cc455a3e34962bf2c
David <dngames2006@gmail.com>
2019-10-11 20:07:01 -0500

    Using supported webdrivers instead of deprecated phantomJS

--- aut0_myon.py
+++ aut0_myon.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 
-phantomJS_PATH = "C:\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\phantomjs.exe"
-driver = webdriver.PhantomJS(executable_path=phantomJS_PATH)
+PORTAL = "https://my.harmonytx.org"
 
-URL = "https://stackoverflow.com"
-BUTTON_PATH = ""
+browser = input("Firefox or Chrome?: ")
 
-driver.get(URL)
-driver.save_screenshot("test.png")
+if browser == "Firefox":
+    driver = webdriver.Firefox()
+elif browser == "Chrome":
+    driver = webdriver.Chrome()
+else:
+    print("Invalid choice")
+    exit()
+
 driver.close()
