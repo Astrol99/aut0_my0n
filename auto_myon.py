7d3c7d121d4909dbd4d2649fd5e863a9a6baedbe
David <dngames2006@gmail.com>
2019-10-11 21:19:44 -0500

    refactored code and converted to class - username login functional

--- auto_myon.py
+++ auto_myon.py
@@ -3,33 +3,62 @@ import datetime
 import time
 import os
 
-PORTAL = "https://my.harmonytx.org"
-
+# Gives time info on what program is doing for debug purposes
 def debug(debug_info):
     now = datetime.datetime.now()
     debug_time = f"{now.hour}:{now.minute}:{now.second}"
     print(f"[DEBUG {debug_time}] {debug_info}")
 
-def driver_path(browser):
-    if browser == "firefox":
-        driver = webdriver.Firefox(executable_path=os.path.abspath("./web_drivers/geckodriver.exe"))
-    elif browser == "chrome":
-        driver = webdriver.Chrome(executable_path=os.path.abspath("./web_drivers/chromedriver.exe"))
-    else:
-        print("[-] Invalid Browser Type! Exiting...")
-        exit()
+class auto_myon:
+    def __init__(self, browser, username, password):
+        self.PORTAL = "https://my.harmonytx.org"
+        self.BROWSER = browser
+        self.USERNAME = username
+        self.PASSWORD = password
 
-    return driver
+    # Decides which driver to use
+    def driver_path(self, browser):
+        if browser == "firefox":
+            driver = webdriver.Firefox(executable_path=os.path.abspath("./web_drivers/geckodriver.exe"))
+        elif browser == "chrome":
+            driver = webdriver.Chrome(executable_path=os.path.abspath("./web_drivers/chromedriver.exe"))
+        else:
+            print("[-] Invalid Browser Type! Exiting...")
+            exit()
 
-def aut0_myon():
-    browser = input(f"[!] Firefox or Chrome?: ").lower()
-    driver = driver_path(browser)
+        return driver
 
-    debug("Logging into portal...")
-    driver.get(PORTAL)
-    time.sleep(3)
+    def login(self):
+        # Username part
+        debug("Finding username input box")
+        username_box = self.driver.find_element_by_id("identification")
+        debug("Entering login credentials")
+        username_box.send_keys(USERNAME)
+        debug("Submitting username...")
+        username_submit_btn = self.driver.find_element_by_id("authn-go-button")
+        username_submit_btn.click()
 
-    driver.close()
+        # Password part
+
+
+    def read(self):
+        debug("Opening browser...")
+        self.driver = self.driver_path(BROWSER)
+
+        # Goes to harmony portal to log in through clever then myon
+        debug("Going into portal...")
+        self.driver.get(self.PORTAL)
+
+        self.login()
+
+        time.sleep(10)
+
+        self.driver.close()
 
 if __name__ == '__main__':
-    aut0_myon()
+    BROWSER = input(f"[!] Firefox or Chrome?: ").lower()
+    USERNAME = input(f"[!] Username: ")
+    PASSWORD = input(f"[!] Password: ")
+
+    myon = auto_myon(BROWSER, USERNAME, PASSWORD)
+    myon.read()
